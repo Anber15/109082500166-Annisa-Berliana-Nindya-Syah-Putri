70b0efaed3612ae7763e1409efa4706899707616
Anber15 <130133923+Anber15@users.noreply.github.com>
2026-04-08 13:50:11 +0700

    Add files via upload

---
+++ Week 5/Unguided/unguided-no1/unguidedno1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func polaBintang(n int) {
+	if n == 0 {
+		return
+	}
+
+	polaBintang(n - 1)
+
+	for i := 0; i < n; i++ {
+		fmt.Print("*")
+	}
+	fmt.Println()
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	polaBintang(n)
+}
